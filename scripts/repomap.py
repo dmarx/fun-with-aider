f197fea3b02650fcd672e4a2f4ce13e88044e582
David Marx <david.marx84@gmail.com>
2025-01-19 13:42:50 -0800

    Update repomap.py

--- scripts/repomap.py
+++ scripts/repomap.py
@@ -30,7 +30,7 @@ map = RepoMap()
 #repo_content = map.get_ranked_tags_map(chat_fnames=all_files, force_refresh=True) # self.io.tool_warning(f"Repo-map can't include {fname}")
 #all_files = [str(f.absolute()) for f in all_files]
 all_files = [str(f.absolute()) for f in all_files if f.is_file()]
-all_files = [f for f in all_files if not any(bad in f) for bad in (".git",".aider")]
+all_files = [f for f in all_files if not any([bad in f for bad in (".git",".aider")])]
 print(all_files)
 #repo_content = map.get_ranked_tags_map(chat_fnames=all_files, force_refresh=True) # fails without a `main_model` to query max token count for
 repo_content = map.get_ranked_tags(chat_fnames=all_files, other_fnames=list(), mentioned_fnames=set(),mentioned_idents=set(),)
